f581bf00921fe0465d0def24e130974ec4ba648f
fab4u_4empion <reserv390@gmail.com>
2022-07-08 13:52:12 +0300

    basic layout

--- src/Clock/Clock.tsx
+++ src/Clock/Clock.tsx
@@ -1,14 +1,30 @@
 import React, { FC } from "react";
-import './styles/index.css'
+import './styles/index.scss'
 import { ClockProps } from "./interfaces/clockPropsInterface";
 
 export const Clock: FC<ClockProps> = ({size}) => {
-    const clockClasses: string[] = ['clock']
-    clockClasses.push(size)
+
+    const clockContentClasses: string[] = ["clock-content"]
+    clockContentClasses.push(size)
 
     return (
-        <div className={clockClasses.join(" ")}>
-
+        <div className="clock-wrapper">
+            <div className={clockContentClasses.join(" ")}>
+                <div className="clock-date">
+                    <div>
+                        пятница
+                    </div>
+                    <div>
+                        8 сентября 2022
+                    </div>
+                </div>
+                <div className="clock-face">
+                    21:05:06
+                </div>
+                <div className="clock-format">
+                    PM
+                </div>
+            </div>
         </div>
     )
 }
